d12e9057874dcc642d819f3e76ee9a4ac6ef112f
Sergio Luis Segura Vidal <sergio31.96@outlook.com>
2018-04-23 21:43:46 -0600

    initialize the project

---
+++ Emily/Emily/Business/AgendaRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emily.Business
+{
+    public class AgendaRepository
+    {
+        public void CreateEvent ()
+        {
+
+        }
+
+        public void UpdateEvent()
+        {
+
+        }
+
+        public void DeleteEvent(int id)
+        {
+
+        }
+
+        public void ShowEvents()
+        {
+
+        }
+    }
+}
---
+++ Emily/Emily/Business/InformationRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emily.Business
+{
+    public class InformationRepository
+    {
+        public DateTime GetHour ()
+        {
+            return DateTime.Now;
+        }
+
+        public string GetWeather()
+        {
+            return String.Empty;
+        }
+    }
+}
