0cbf0f2070eabbf7d61f0e0aef0676a37d4df306
Sergio S <sergio31.96@outlook.com>
2020-12-17 16:09:22 -0600

    Added the delete function

--- Emmily/urls.py
+++ Emmily/urls.py
@@ -6,5 +6,7 @@ import wordlist_skill.api_views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/v1/wordlist/',wordlist_skill.api_views.WordList.as_view())
+    path('api/v1/wordlist/',wordlist_skill.api_views.WordList.as_view()),
+    path('api/v1/wordlist/new',wordlist_skill.api_views.WordCreation.as_view()),
+    path('api/v1/wordlist/delete/<int:id>',wordlist_skill.api_views.WordDestroy.as_view())
 ]
--- wordlist_skill/api_views.py
+++ wordlist_skill/api_views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
@@ -6,7 +7,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from wordlist_skill.serializers import WordSerializer
 from wordlist_skill.models import Word
 
-class WordsPagination(LimitOffsetPagination):
+class WordPagination(LimitOffsetPagination):
     default_limit = 10
     max_limit = 100
 
@@ -16,7 +17,7 @@ class WordList(ListAPIView):
     filter_backends = (DjangoFilterBackend,SearchFilter)
     filter_fields = ('id',)
     search_fields = ('word',"definition")
-    pagination_class = WordsPagination
+    pagination_class = WordPagination
 
     def get_queryset(self):
             #Filter by language
@@ -29,3 +30,32 @@ class WordList(ListAPIView):
             return queryset
 
 
+class WordCreation(CreateAPIView):
+    serializer_class = WordSerializer
+    
+    def create(self,request,*args,**kwargs):
+        #Check the aren't on the DB
+        
+        #Give format to the fields
+        request.data["word"] = request.data.get("name").capitalize()
+        request.data["definition"] = request.data.get("definition").capitalize()
+        request.data["example"] = request.data.get("example").capitalize()
+
+        request_word = request.data.get("word")
+        if Word.objects.filter(word = request_word).count() != 0:
+            raise ValidationError({'word':'Already existe on the DB'})
+        return super().create(request,*args,**kwargs)
+    
+
+class WordDestroy(DestroyAPIView):
+    queryset = Word.objects.all()
+    lookup_field = 'id'
+
+    def delete(self,request,*args, **kwargs):
+        #Clear cache
+        product_id = request.data.get('id')
+        response = super().delete(self,request,*args, **kwargs)
+        if response.status_code == 204:
+            from django.core.cache import cache
+            cache.delete('product_data_{}'.format(product_id))
+        return response
