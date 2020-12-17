55463d65e5b6df4c535857f1b4bb581418aabc42
Sergio S <sergio31.96@outlook.com>
2020-12-17 12:25:39 -0600

    Update migration

--- wordlist_skill/models.py
+++ wordlist_skill/models.py
@@ -5,5 +5,5 @@ class Word(models.Model):
     word = models.CharField(max_length=100)
     phonetic = models.CharField(max_length=100)
     language = models.CharField(max_length=100)
-    definition = models.TextField(max_length=100, blank=True)
-    example = models.TextField(max_length=100, blank=True)
+    definition = models.TextField(max_length=256, blank=True)
+    example = models.TextField(max_length=256, blank=True)
