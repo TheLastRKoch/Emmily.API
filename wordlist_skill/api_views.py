dc6c54d0d768b1e54277b67ce28f6a44565e9362
Sergio S <sergio31.96@outlook.com>
2020-12-17 14:17:21 -0600

    Added limit and pagination

--- wordlist_skill/api_views.py
+++ wordlist_skill/api_views.py
@@ -1,9 +1,14 @@
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 from wordlist_skill.serializers import WordSerializer
 from wordlist_skill.models import Word
+
+class WordsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
 class WordList(ListAPIView):
     queryset = Word.objects.all()
@@ -11,6 +16,7 @@ class WordList(ListAPIView):
     filter_backends = (DjangoFilterBackend,SearchFilter)
     filter_fields = ('id',)
     search_fields = ('word',"definition")
+    pagination_class = WordsPagination
 
     def get_queryset(self):
             #Filter by language
@@ -21,4 +27,5 @@ class WordList(ListAPIView):
                     language=request_language
                 )
             return queryset
+
 
