fb96b08dc72553ea829ade5be13268af486cfdb4
Sergio S <sergio31.96@outlook.com>
2021-02-20 15:14:47 -0600

    Migrate Word from generic views to function base views

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from vocabulary.views import *
-from vocabulary.api_views import *
+from rest_framework import views
+from . import views
 
 app_name = 'vocabulary'
 
 urlpatterns = [
     # Words manage
-	path('word', WordGetList.as_view() , name="list"),
-	path('word/new', WordCreation.as_view(), name="create"),
-	path('word/<int:id>', WordRetriveUpdateDestroy.as_view(), name="manage"),
+	path('word',views.getWords , name="list"),
+	path('word/<int:id>',views.WordRetriveUpdateDestroy , name="detail"),
+	path('word/new', views.addNewWord, name="create"),
 
 	# WordList manage
-	path('wordlist', WordListGetList.as_view() , name="list"),
-	path('wordlist/new', WordListCreation.as_view(), name="create"),
-	path('wordlist/<int:id>', WordListRetriveUpdateDestroy.as_view(), name="manage")
+	#path('wordlist', WordListGetList.as_view() , name="list"),
+	#path('wordlist/new', WordListCreation.as_view(), name="create"),
+	#path('wordlist/<int:id>', WordListRetriveUpdateDestroy.as_view(), name="manage")
 ]
--- vocabulary/views.py
+++ vocabulary/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.db.models import Q
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Word
+from .serializers import WordSerializer
+from rest_framework.exceptions import ValidationError
 
-def test (resquest):
-    return JsonResponse({'response':'This is a test'})
+#region Word
+@api_view(['GET'])
+def getWords(request):
+    words = Word.objects.all()
+    serializer = WordSerializer(words, many=True)
+    return Response (serializer.data)
+
+@api_view(['POST'])
+def addNewWord(request):
+    serializer = WordSerializer(data=request.data)
+    if Word.objects.filter(word = request.data['word']).count() > 0:
+        raise ValidationError({'detail': 'The requested word already exist on the DB'})
+    if serializer.is_valid():
+        serializer.save()
+    return Response (serializer.data)
+
+@api_view(['GET','PUT','DELETE'])
+def WordRetriveUpdateDestroy(request,id):
+    if request.method == 'GET':
+        word = Word.objects.get(id=id)
+        serializer = WordSerializer(word, many=False)
+        return Response (serializer.data)
+
+    if request.method == 'PUT':
+        word = Word.objects.get(id=id)
+        serializer = WordSerializer(instance=word,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response (serializer.data)
+    
+    if request.method == 'DELETE':
+        word = Word.objects.get(id=id)
+        word.delete()
+        return Response({'detail': 'Word deleted successfully'})
+#endregion
